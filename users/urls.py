fd1141b4dbc728664d4829069c1396ae72695293
Luiz Bessa <lbessaregis@gmail.com>
2024-11-11 17:20:47 -0300

    Add signup and signin preview
    NOTE: not finished, just for the application to work.

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("signin", views.signin, name="signin"),
+    path("signup", views.signup, name="signup"),
     path("real-state-financing/", views.real_state_financing, name="real_state_financing"),
 ]
--- users/views.py
+++ users/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 def index(request):
     return render(request, "index.html")
 
+def signin(request):
+    return render(request, "signin.html")
+
+def signup(request):
+    return render(request, "signup.html")
+
 def real_state_financing():
     return render('financing.html')
